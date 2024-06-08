40b5b27ecf609989b816028fcf262a3d62c2c1f6
Rangga Aprilio Utama <ranggaaprillio@gmail.com>
2024-06-08 20:07:49 +0700

    AuthOutlet

--- src/routes/protectedRoutes.tsx
+++ src/routes/protectedRoutes.tsx
@@ -5,33 +5,33 @@ import { Suspense } from "react";
 import { Navigate } from "react-router-dom";
 
 const { Dashboard } = lazyImport(
-  () => import("@/features/dashboard"),
-  "Dashboard",
+	() => import("@/features/dashboard"),
+	"Dashboard",
 );
 
 const App = () => {
-  return (
-    <MainLayout>
-      <Suspense
-        fallback={
-          <div className="h-full w-full flex items-center justify-center">
-            Loading ..
-          </div>
-        }
-      >
-        <AuthOutlet fallbackPath="/auth/login" />
-      </Suspense>
-    </MainLayout>
-  );
+	return (
+		<MainLayout>
+			<Suspense
+				fallback={
+					<div className="h-full w-full flex items-center justify-center">
+						Loading ..
+					</div>
+				}
+			>
+				<AuthOutlet fallbackPath="/auth/login" />
+			</Suspense>
+		</MainLayout>
+	);
 };
 
 export const protectedRoutes = [
-  {
-    path: "/app",
-    element: <App />,
-    children: [
-      { path: "", element: <Dashboard /> },
-      { path: "*", element: <Navigate to="." /> },
-    ],
-  },
+	{
+		path: "/app",
+		element: <App />,
+		children: [
+			{ path: "", element: <Dashboard /> },
+			{ path: "*", element: <Navigate to="." /> },
+		],
+	},
 ];
