35d715a2617cfef86d938b2fe1a3b5fcc1b49b50
Rangga Aprilio Utama <ranggaaprillio@gmail.com>
2024-06-08 20:07:27 +0700

    install jest

--- src/routes/protectedRoutes.tsx
+++ src/routes/protectedRoutes.tsx
@@ -2,36 +2,36 @@ import { MainLayout } from "@/components/Layout";
 import { lazyImport } from "@/utils/lazyImport";
 import AuthOutlet from "@auth-kit/react-router/AuthOutlet";
 import { Suspense } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const { Dashboard } = lazyImport(
-	() => import("@/features/dashboard"),
-	"Dashboard",
+  () => import("@/features/dashboard"),
+  "Dashboard",
 );
 
 const App = () => {
-	return (
-		<MainLayout>
-			<Suspense
-				fallback={
-					<div className="h-full w-full flex items-center justify-center">
-						Loading ..
-					</div>
-				}
-			>
-				<AuthOutlet fallbackPath="/auth/login" />
-			</Suspense>
-		</MainLayout>
-	);
+  return (
+    <MainLayout>
+      <Suspense
+        fallback={
+          <div className="h-full w-full flex items-center justify-center">
+            Loading ..
+          </div>
+        }
+      >
+        <AuthOutlet fallbackPath="/auth/login" />
+      </Suspense>
+    </MainLayout>
+  );
 };
 
 export const protectedRoutes = [
-	{
-		path: "/app",
-		element: <App />,
-		children: [
-			{ path: "", element: <Dashboard /> },
-			{ path: "*", element: <Navigate to="." /> },
-		],
-	},
+  {
+    path: "/app",
+    element: <App />,
+    children: [
+      { path: "", element: <Dashboard /> },
+      { path: "*", element: <Navigate to="." /> },
+    ],
+  },
 ];
